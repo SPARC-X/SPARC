7eaaa4746f4945b82cdf839663a1acc2f0117f08
Luke Erlandson <LukeAErlandson@gmail.com>
2021-03-19 01:03:27 -0500

    NonLocal Tests

--- tests/quick/testing/debug.sh
+++ tests/quick/testing/debug.sh
@@ -2,6 +2,6 @@
 
 # mpirun -np 6 gdb -batch -ex=r -ex=bt -ex=q --args ./sparc -name BaTiO3
 #LIBPCE_DEBUG_LEVEL=5 mpirun -np 1 gdb -batch -ex=r -ex=bt -ex=q --args ./sparc -name BaTiO3
-#LIBPCE_DEBUG_LEVEL=5 mpirun -np 1 valgrind ./sparc -name BaTiO3
+#LIBPCE_DEBUG_LEVEL=5 mpirun -np 1 valgrind --track-origins=yes ./sparc -name BaTiO3
 #LIBPCE_DEBUG_LEVEL=25 mpirun -hostfile hostfile --np 16 gdb -batch -ex=r -ex=bt -ex=q --args ./sparc -name BaTiO3 | tee serial_no_them.txt
-LIBPCE_DEBUG_LEVEL=25 mpirun -hostfile hostfile --np 1 ./sparc -name BaTiO3 | tee serial_no_them.txt
+LIBPCE_DEBUG_LEVEL=25 mpirun -hostfile hostfile --np 4 gdb -batch -ex=r -ex=bt -ex=q --args ./sparc -name BaTiO3 | tee nl_ser.txt
