1c1bb741201b2db126eab54b78837dd9d2b836b6
Qimen Xu <qimenxu@foxmail.com>
2022-07-14 16:31:46 -0400

    add hard-coded order orth Lap stencils

--- src/lapVecOrth.c
+++ src/lapVecOrth.c
@@ -83,7 +83,7 @@ void Lap_vec_mult_orth(
  *
  * Copyright (c) 2018-2019 Edmond Group at Georgia Tech.
  */
-void stencil_3axis_thread_v2(
+void stencil_3axis_thread_variable_radius(
     const double *x0,    const int radius, 
     const int stride_y,  const int stride_y_ex, 
     const int stride_z,  const int stride_z_ex,
@@ -128,9 +128,143 @@ void stencil_3axis_thread_v2(
 }
 
 
+void stencil_3axis_thread_radius6(
+    const double *x0,    const int radius, 
+    const int stride_y,  const int stride_y_ex, 
+    const int stride_z,  const int stride_z_ex,
+    const int x_spos,    const int x_epos, 
+    const int y_spos,    const int y_epos,
+    const int z_spos,    const int z_epos,
+    const int x_ex_spos, const int y_ex_spos,  // this allows us to give x as x0 for 
+    const int z_ex_spos,                       // calc inner part of Lx
+    const double *stencil_coefs, 
+    const double coef_0, const double b,   
+    const double *v0,    double *y
+)
+{
+    int i, j, k, jp, kp, r;
+    const int shift_ip = x_ex_spos - x_spos;
+    for (k = z_spos, kp = z_ex_spos; k < z_epos; k++, kp++)
+    {
+        for (j = y_spos, jp = y_ex_spos; j < y_epos; j++, jp++)
+        {
+            int offset = k * stride_z + j * stride_y;
+            int offset_ex = kp * stride_z_ex + jp * stride_y_ex;
+            #pragma omp simd
+            for (i = x_spos; i < x_epos; i++)
+            {
+                int ip     = i + shift_ip;
+                int idx    = offset + i;
+                int idx_ex = offset_ex + ip;
+                double res = coef_0 * x0[idx_ex];
+                for (r = 1; r <= 6; r++)
+                {
+                    int stride_y_r = r * stride_y_ex;
+                    int stride_z_r = r * stride_z_ex;
+                    double res_x = (x0[idx_ex - r]          + x0[idx_ex + r])          * stencil_coefs[3*r];
+                    double res_y = (x0[idx_ex - stride_y_r] + x0[idx_ex + stride_y_r]) * stencil_coefs[3*r+1];
+                    double res_z = (x0[idx_ex - stride_z_r] + x0[idx_ex + stride_z_r]) * stencil_coefs[3*r+2];
+                    res += res_x + res_y + res_z;
+                }
+                y[idx] = res + b * (v0[idx] * x0[idx_ex]); 
+            }
+        }
+    }
+}
+
+
+void stencil_3axis_thread_radius4(
+    const double *x0,    const int radius, 
+    const int stride_y,  const int stride_y_ex, 
+    const int stride_z,  const int stride_z_ex,
+    const int x_spos,    const int x_epos, 
+    const int y_spos,    const int y_epos,
+    const int z_spos,    const int z_epos,
+    const int x_ex_spos, const int y_ex_spos,  // this allows us to give x as x0 for 
+    const int z_ex_spos,                       // calc inner part of Lx
+    const double *stencil_coefs, 
+    const double coef_0, const double b,   
+    const double *v0,    double *y
+)
+{
+    int i, j, k, jp, kp, r;
+    const int shift_ip = x_ex_spos - x_spos;
+    for (k = z_spos, kp = z_ex_spos; k < z_epos; k++, kp++)
+    {
+        for (j = y_spos, jp = y_ex_spos; j < y_epos; j++, jp++)
+        {
+            int offset = k * stride_z + j * stride_y;
+            int offset_ex = kp * stride_z_ex + jp * stride_y_ex;
+            #pragma omp simd
+            for (i = x_spos; i < x_epos; i++)
+            {
+                int ip     = i + shift_ip;
+                int idx    = offset + i;
+                int idx_ex = offset_ex + ip;
+                double res = coef_0 * x0[idx_ex];
+                for (r = 1; r <= 4; r++)
+                {
+                    int stride_y_r = r * stride_y_ex;
+                    int stride_z_r = r * stride_z_ex;
+                    double res_x = (x0[idx_ex - r]          + x0[idx_ex + r])          * stencil_coefs[3*r];
+                    double res_y = (x0[idx_ex - stride_y_r] + x0[idx_ex + stride_y_r]) * stencil_coefs[3*r+1];
+                    double res_z = (x0[idx_ex - stride_z_r] + x0[idx_ex + stride_z_r]) * stencil_coefs[3*r+2];
+                    res += res_x + res_y + res_z;
+                }
+                y[idx] = res + b * (v0[idx] * x0[idx_ex]); 
+            }
+        }
+    }
+}
+
+
+void stencil_3axis_thread_radius8(
+    const double *x0,    const int radius, 
+    const int stride_y,  const int stride_y_ex, 
+    const int stride_z,  const int stride_z_ex,
+    const int x_spos,    const int x_epos, 
+    const int y_spos,    const int y_epos,
+    const int z_spos,    const int z_epos,
+    const int x_ex_spos, const int y_ex_spos,  // this allows us to give x as x0 for 
+    const int z_ex_spos,                       // calc inner part of Lx
+    const double *stencil_coefs, 
+    const double coef_0, const double b,   
+    const double *v0,    double *y
+)
+{
+    int i, j, k, jp, kp, r;
+    const int shift_ip = x_ex_spos - x_spos;
+    for (k = z_spos, kp = z_ex_spos; k < z_epos; k++, kp++)
+    {
+        for (j = y_spos, jp = y_ex_spos; j < y_epos; j++, jp++)
+        {
+            int offset = k * stride_z + j * stride_y;
+            int offset_ex = kp * stride_z_ex + jp * stride_y_ex;
+            #pragma omp simd
+            for (i = x_spos; i < x_epos; i++)
+            {
+                int ip     = i + shift_ip;
+                int idx    = offset + i;
+                int idx_ex = offset_ex + ip;
+                double res = coef_0 * x0[idx_ex];
+                for (r = 1; r <= 8; r++)
+                {
+                    int stride_y_r = r * stride_y_ex;
+                    int stride_z_r = r * stride_z_ex;
+                    double res_x = (x0[idx_ex - r]          + x0[idx_ex + r])          * stencil_coefs[3*r];
+                    double res_y = (x0[idx_ex - stride_y_r] + x0[idx_ex + stride_y_r]) * stencil_coefs[3*r+1];
+                    double res_z = (x0[idx_ex - stride_z_r] + x0[idx_ex + stride_z_r]) * stencil_coefs[3*r+2];
+                    res += res_x + res_y + res_z;
+                }
+                y[idx] = res + b * (v0[idx] * x0[idx_ex]); 
+            }
+        }
+    }
+}
+
 
 /**
- * @brief   Kernel for calculating y = (a * Lap + b * diag(v0) + c * I) * x + beta * y.
+ * @brief   Kernel for calculating y = (a * Lap + b * diag(v0) + c * I) * x.
  *          For the input & output domain, z/x index is the slowest/fastest running index
  *
  * @param x0               : Input domain with extended boundary 
@@ -155,14 +289,8 @@ void stencil_3axis_thread_v2(
  * @param beta             : Scaling factor of y
  * @param y (OUT)          : Output domain with original boundary
  *
- * @author  Hua Huang <huangh223@gatech.edu>
- *          Edmond Chow <echow@cc.gatech.edu>
- *
- * @modified by Qimen Xu <qimenxu@gatech.edu>, Mar 2019, Georgia Tech
- *
- * Copyright (c) 2018-2019 Edmond Group at Georgia Tech.
  */
-void stencil_3axis_thread_v1(
+void stencil_3axis_thread_v2(
     const double *x0,    const int radius, 
     const int stride_y,  const int stride_y_ex, 
     const int stride_z,  const int stride_z_ex,
@@ -173,38 +301,47 @@ void stencil_3axis_thread_v1(
     const int z_ex_spos,                       // calc inner part of Lx
     const double *stencil_coefs, 
     const double coef_0, const double b,   
-    const double *v0,    const double beta, 
-    double *y
+    const double *v0,    double *y
 )
 {
-    int i, j, k, jp, kp, r;
-    const int shift_ip = x_ex_spos - x_spos;
-    for (k = z_spos, kp = z_ex_spos; k < z_epos; k++, kp++)
+    switch (radius)
     {
-        for (j = y_spos, jp = y_ex_spos; j < y_epos; j++, jp++)
-        {
-            int offset = k * stride_z + j * stride_y;
-            int offset_ex = kp * stride_z_ex + jp * stride_y_ex;
-            #pragma omp simd
-            for (i = x_spos; i < x_epos; i++)
-            {
-                int ip     = i + shift_ip;
-                int idx    = offset + i;
-                int idx_ex = offset_ex + ip;
-                double res = coef_0 * x0[idx_ex];
-                for (r = 1; r <= radius; r++)
-                {
-                    int stride_y_r = r * stride_y_ex;
-                    int stride_z_r = r * stride_z_ex;
-                    double res_x = (x0[idx_ex - r]          + x0[idx_ex + r])          * stencil_coefs[3*r];
-                    double res_y = (x0[idx_ex - stride_y_r] + x0[idx_ex + stride_y_r]) * stencil_coefs[3*r+1];
-                    double res_z = (x0[idx_ex - stride_z_r] + x0[idx_ex + stride_z_r]) * stencil_coefs[3*r+2];
-                    res += res_x + res_y + res_z;
-                }
-                y[idx] = res + b * (v0[idx] * x0[idx_ex]) + beta*y[idx];
-            }
-        }
-    }    
+        case 4:
+            stencil_3axis_thread_radius4(
+                x0, radius, stride_y,  stride_y_ex, stride_z, stride_z_ex,
+                x_spos, x_epos, y_spos, y_epos, z_spos, z_epos, x_ex_spos, y_ex_spos, z_ex_spos,
+                stencil_coefs, coef_0, b, v0, y
+            );
+            return;
+            break;
+
+        case 6:
+            stencil_3axis_thread_radius6(
+                x0, radius, stride_y,  stride_y_ex, stride_z, stride_z_ex,
+                x_spos, x_epos, y_spos, y_epos, z_spos, z_epos, x_ex_spos, y_ex_spos, z_ex_spos,
+                stencil_coefs, coef_0, b, v0, y
+            );
+            return;
+            break;
+
+        case 8:
+            stencil_3axis_thread_radius8(
+                x0, radius, stride_y,  stride_y_ex, stride_z, stride_z_ex,
+                x_spos, x_epos, y_spos, y_epos, z_spos, z_epos, x_ex_spos, y_ex_spos, z_ex_spos,
+                stencil_coefs, coef_0, b, v0, y
+            );
+            return;
+            break;
+
+        default:
+            stencil_3axis_thread_variable_radius(
+                x0, radius, stride_y,  stride_y_ex, stride_z, stride_z_ex,
+                x_spos, x_epos, y_spos, y_epos, z_spos, z_epos, x_ex_spos, y_ex_spos, z_ex_spos,
+                stencil_coefs, coef_0, b, v0, y
+            );
+            return;
+            break;
+    }
 }
 
 
